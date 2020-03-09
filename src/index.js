e74b48ea1c80e55afcd2375cb73dec1e8b27149c
Andrei Chapiuk <venik2281488@gmail.ru>
2020-03-10 00:33:02 +0300

    complite:'reverse-int'-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+let numSrt = String(number)
+let numLen = numSrt.length
+let answer = [];
+if (numLen == 4 || numLen == 3 ) { 
+  if (numSrt[0] == '-' ) {
+answer.push(numSrt[3])
+answer.push(numSrt[2])
+answer.push(numSrt[1])
+let nu = answer.join('')
+return Number(nu);
+  }
+  if (numSrt[0] !== '-' ) {
+    answer.push(numSrt[2])
+    answer.push(numSrt[1])
+    answer.push(numSrt[0])
+let nu = answer.join('')
+return Number(nu);
+      }
+}
 }
